c32722ed90f668eaf373da4ecaec4a3f89f9756f
Justincadburywong <justincadburywong@gmail.com>
2016-11-27 11:32:08 -0800

    add constantsgs

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,10 @@ APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
 
+TWILIO_SID = ENV['twilio_sid']
+TWILIO_TOKEN = ENV['twilio_token']
+TWILIO_NUMBER = ENV['twilio_number']
+
 configure do
   # By default, Sinatra assumes that the root is the file that calls the configure block.
   # Since this is not the case for us, we set it manually.
