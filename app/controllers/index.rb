69cd7739a984d2fd2ee8b591d5f79ba0f5c8946e
Justincadburywong <justincadburywong@gmail.com>
2016-11-27 19:12:36 -0800

    add debug

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,10 +1,15 @@
 post '/' do
   body = params["Body"]
+  p body
+  p body.downcase
   if body.downcase.include? 'nerdy'
+    p 'nerdy'
     send_nerdy_text
   elsif body.downcase.include? 'dirty'
+    p 'dirty'
     send_dirty_text
   else
+    p 'normal'
     send_joke_text
   end
 end
