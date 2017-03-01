9a9153ec75d782a811710f3b19dfcdd805aa14d9
Justincadburywong <justincadburywong@gmail.com>
2017-03-01 10:52:36 -0800

    remove voice controller

--- app/controllers/voice.rb
+++ app/controllers/voice.rb
@@ -1,20 +1,20 @@
-post '/voice' do
-  p params
-  if params['From'] == '+14158459551'
-    # answer the call
-    gather_number  # ask for phone number
-  else
-    # answer the call
-    redirect '/voice/failed'
-  end
-end
-
-get '/voice/failed' do
-  failed
-end
-
-post '/voice/forward' do
-  p params
-  @collected_number = params['Digits']
-  forward_call
-end
+# post '/voice' do
+#   p params
+#   if params['From'] == '+14158459551'
+#     # answer the call
+#     gather_number  # ask for phone number
+#   else
+#     # answer the call
+#     redirect '/voice/failed'
+#   end
+# end
+#
+# get '/voice/failed' do
+#   failed
+# end
+#
+# post '/voice/forward' do
+#   p params
+#   @collected_number = params['Digits']
+#   forward_call
+# end
