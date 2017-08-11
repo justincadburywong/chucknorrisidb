33dd536c3d41ebc401545924275fc13c5b6d65e5
Justincadburywong <justincadburywong@gmail.com>
2017-08-10 21:00:00 -0700

    update routes to add /scrape

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -23,3 +23,17 @@ get '/success' do
   @message = "Message sent!"
   erb :'chuck'
 end
+
+get '/scrape' do
+  erb :'scrape'
+end
+
+post '/scrape' do
+  scrape_api
+  redirect '/scrape_success'
+end
+
+get '/scrape_success' do
+  @message = "Scrape succeeded!"
+  erb :'scrape'
+end
