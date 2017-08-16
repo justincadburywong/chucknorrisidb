f6552117345deb2107cf8d6d8dfbddcf94e38b57
Justincadburywong <justincadburywong@gmail.com>
2017-08-15 21:41:12 -0700

    add stubbed external HTTP requests for API

--- spec/icndb_request_spec.rb
+++ spec/icndb_request_spec.rb
@@ -1,15 +1,61 @@
 require 'spec_helper'
+require 'json'
 
 describe 'External request' do
   context 'From ICNDB' do
+    before(:each) do
+      stub_request(:get, /api.icndb.com/).
+        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'api.icndb.com', 'User-Agent'=>'Ruby' }).
+        to_return(status: 200, body: {type: 'success', value: {id: 454, joke: "Chuck Norris's first program was kill -9.", categories: ['nerdy']}}.to_json, headers: { 'Content-Type': 'application/json' })
+    end
+
     uri = URI('https://api.icndb.com/jokes/random')
     let(:response) { Net::HTTP.get_response(uri) }
 
-    it 'queries ICNDB jokes' do
+    it 'returns a JSON object' do
       expect(response.body).to be_an_instance_of(String)
     end
     it 'has a valid response code' do
       expect(response.code).to eq("200")
+    end
+    it 'has a valid id datatype' do
+      expect(JSON.parse(response.body)['value']['id']).to be_an_instance_of(Integer)
+    end
+    it 'has a valid id' do
+      expect(JSON.parse(response.body)['value']['id']).to eq(454)
+    end
+    it 'has a valid joke datatype' do
+      expect(JSON.parse(response.body)['value']['joke']).to be_an_instance_of(String)
+    end
+    it 'has a valid joke' do
+      expect(JSON.parse(response.body)['value']['joke']).to eq("Chuck Norris's first program was kill -9.")
+    end
+    it 'has a category array' do
+      expect(JSON.parse(response.body)['value']['categories']).to be_an_instance_of(Array)
+    end
+    it 'has a valid category' do
+      expect(JSON.parse(response.body)['value']['categories'].first).to eq('nerdy')
+    end
+  end
+
+  context 'Failed ICNDB' do
+    before(:each) do
+      stub_request(:get, /api.icndb.com/).
+        with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+        to_return(status: 500, body: "stubbed response", headers: {})
+    end
+
+    uri = URI('https://api.icndb.com/jokes/random')
+    let(:response) { Net::HTTP.get_response(uri) }
+
+    it 'has a valid response code' do
+      expect(response.code).to eq("500")
+    end
+    it 'defaults to database' do
+      # expect(response.body).to eq("stubbed response")
+    end
+    it 'successfully retrieves a joke from the databse' do
+      # expect(response.body).to be_an_instance_of(Array)
     end
   end
 end
