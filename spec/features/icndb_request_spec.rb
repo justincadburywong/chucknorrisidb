db2f8d0ba4c7a6c1b5913539c6cda3bac9bbdb62
Justincadburywong <justincadburywong@gmail.com>
2017-08-25 00:03:46 -0700

    remove badly written test

--- spec/features/icndb_request_spec.rb
+++ spec/features/icndb_request_spec.rb
@@ -18,20 +18,20 @@ describe 'External request' do
     it 'has a valid response code' do
       expect(response.code).to eq("200")
     end
-    # it 'has a valid id datatype' do
-    #   expect(JSON.parse(response.body)['value']['id']).to be_an_instance_of(Integer)
-    # end
+    it 'has a valid id datatype' do
+      expect(JSON.parse(response.body)['value']['id']).to be_an Integer
+    end
     it 'has a valid id' do
       expect(JSON.parse(response.body)['value']['id']).to eq(454)
     end
     it 'has a valid joke datatype' do
-      expect(JSON.parse(response.body)['value']['joke']).to be_an_instance_of(String)
+      expect(JSON.parse(response.body)['value']['joke']).to be_a String
     end
     it 'has a valid joke' do
       expect(JSON.parse(response.body)['value']['joke']).to eq("Chuck Norris's first program was kill -9.")
     end
     it 'has a category array' do
-      expect(JSON.parse(response.body)['value']['categories']).to be_an_instance_of(Array)
+      expect(JSON.parse(response.body)['value']['categories']).to be_an Array
     end
     it 'has a valid category' do
       expect(JSON.parse(response.body)['value']['categories'].first).to eq('nerdy')
@@ -47,15 +47,19 @@ describe 'External request' do
 
     uri = URI('https://api.icndb.com/jokes/random')
     let(:response) { Net::HTTP.get_response(uri) }
+    let(:joke){ Joke.new(joke: "bunch of stuff", categories: ["nerdy"]) }
+    let(:route){ post '/', :From => 4155552200, :Body => "Nerdy" }
 
     it 'has a valid response code' do
       expect(response.code).to eq("500")
     end
-    it 'defaults to database' do
-      # expect(response.body).to eq("stubbed response")
-    end
-    it 'successfully retrieves a joke from the databse' do
-      # expect(response.body).to be_an_instance_of(Array)
-    end
+    # it 'defaults to database' do
+    #   expect(route).to receive(:read_joke)
+    #   route.fetch_joke
+    # end
+    # it 'successfully retrieves a joke from the databse' do
+    #   # expect(route).to receive(:read_joke)
+    #   # route.fetch_joke
+    # end
   end
 end
